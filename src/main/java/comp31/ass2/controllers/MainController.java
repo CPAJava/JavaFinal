938fef4a3dcf74cbbd32f3efa87b9eb02128ddc8
cAFE-DAP <xuancheng.li@Student.SL.On.Ca>
2023-12-05 14:40:12 -0500

    Added Code to solve null petsOwner issues

--- src/main/java/comp31/ass2/controllers/MainController.java
+++ src/main/java/comp31/ass2/controllers/MainController.java
@@ -288,22 +288,35 @@ public class MainController {
         model.addAttribute("petName", petName);
         //List<Pets> foundPet = staffService.findBypetName(petName);
         //logger.info("pets------", foundPet);
+        model.addAttribute("ownerAll", staffService.findAllOwners());
         if (petName.isEmpty()) {
-            Pet found = new Pet();
-            model.addAttribute("found", found);
-        }
-        else {
-        Pet found = new Pet();
-        found = staffService.findByPetName(petName); 
-        model.addAttribute("found", found);
+          Pet found = new Pet();
+          model.addAttribute("found", found);
+          model.addAttribute("currentOwner", " ");
+        } else {
+          Pet found = new Pet();
+          found = staffService.findByPetName(petName);
+          model.addAttribute("found", found);
+          PetOwner owner = found.getPetOwner();
+          // System.out.println(owner.getUserId());
+          if (owner == null) {
+            model.addAttribute("currentOwner", "-");
+          } else {
+            model.addAttribute("currentOwner", found.getPetOwner().getUserId());
+
+          }
+
         }
         return "updatePets";
     }
 
     @PostMapping("/update-pet")
-    public String updatePetInfo(Model model, Pet newPet) {
+    public String updatePetInfo(Model model, Pet newPet, @RequestParam(defaultValue = "") String ownerId) {
 
         model.addAttribute("found", new Pet());
+        PetOwner updatedPetOwner = staffService.findOwnerByUserId(ownerId);
+        //System.out.println(updatedPetOwner);
+        newPet.setPetOwner(updatedPetOwner);
         staffService.insertPet(newPet);
 
         return "redirect:/find-pet";
